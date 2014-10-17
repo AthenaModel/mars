bdf65d0a7e5b8ca63d253fb9e37c9b08bf0bcbe0
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2014-10-17 16:09:22 -0700

    Metadata update.

--- lib/kiteinfo/kiteinfo.tcl
+++ lib/kiteinfo/kiteinfo.tcl
@@ -19,34 +19,24 @@ namespace eval ::kiteinfo:: {
     variable kiteInfo
 
     array set kiteInfo {
-        reqver-BWidget 1.9
         reqver-Tktable 2.11
-        build-Marsbin {make -f MakeTEA clean all}
-        clean-libGeotrans {make clean}
-        binary-simlib 0
-        binary-marsutil 0
-        apptype-mars kit
-        local-tablelist 0
-        clean-Marsbin {make -f MakeTEA clean}
-        build-libGeostars {make clean all}
-        description {Mars Simulation Support Library}
-        provides {marsutil marsgui simlib Marsbin}
-        local-treectrl 0
-        local-Img 0
         build-libTiff {make clean all}
-        reqver-Tkhtml 3.0
-        local-BWidget 0
+        local-Img 0
         reqver-comm 4.6
-        local-Tktable 0
-        reqver-sqlite3 3.8
         clean-libGTiff {make clean}
+        local-Tktable 0
+        apps mars
+        build-libGTiff {make clean all}
+        local-Tkhtml 0
+        dists install-%platform
+        build-Marsbin {make -f MakeTEA clean all}
+        build-libGeostars {make clean all}
+        local-tablelist 0
+        local-treectrl 0
         clean-libGeostars {make clean}
         reqver-kiteutils 0.4.1
-        reqver-snit 2.3
-        clean-libTiff {make clean}
-        requires {snit sqlite3 comm Img BWidget treectrl tablelist Tktable Tkhtml kiteutils}
-        shell {}
-        distpat-install-win32-ix86 {
+        binary-marsgui 0
+        distpat-install-%platform {
     README.md
     %apps
     %libs
@@ -57,25 +47,36 @@ namespace eval ::kiteinfo:: {
         https://pepper.jpl.nasa.gov/kite/xdocs/mars/3.0/mag-20140826.docx
     }
 }
+        local-snit 0
+        build-libGeotrans {make clean all}
+        reqver-treectrl 2.4
+        reqver-BWidget 1.9
+        clean-libGeotrans {make clean}
+        binary-marsutil 0
+        provides {marsutil marsgui simlib Marsbin}
+        description {Mars Simulation Support Library}
+        local-BWidget 0
+        reqver-sqlite3 3.8
+        shell {}
+        clean-libTiff {make clean}
+        reqver-snit 2.3
         name mars
-        binary-marsgui 0
+        icon-mars {}
+        local-sqlite3 0
+        reqver-tablelist 5.11
+        binary-simlib 0
+        apptype-mars kit
+        clean-Marsbin {make -f MakeTEA clean}
+        reqver-Tkhtml 3.0
+        requires {snit sqlite3 comm Img BWidget treectrl tablelist Tktable Tkhtml kiteutils}
         poc William.H.Duquette@jpl.nasa.gov
         srcs {libGeotrans libGeostars libTiff libGTiff Marsbin}
-        local-Tkhtml 0
         local-comm 0
-        build-libGTiff {make clean all}
-        apps mars
-        local-sqlite3 0
         gui-mars 0
-        local-kiteutils 1
-        reqver-tablelist 5.11
-        local-snit 0
-        binary-Marsbin 1
         version 3.0.2a0
-        reqver-treectrl 2.4
+        binary-Marsbin 1
+        local-kiteutils 1
         reqver-Img 1.4.1
-        build-libGeotrans {make clean all}
-        dists install-win32-ix86
     }
 
     namespace export \
